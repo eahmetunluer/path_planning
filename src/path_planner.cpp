82f523b20580b8aa6d9ec33144bbfb1be8a92441
eahmetunluer <eahmetunluer@gmail.com>
2021-05-30 23:43:22 +0300

    RViz connections has been established

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include "visualization_msgs/Marker.h"
 #include "nav_msgs/Path.h"
+#include "path_planning/input_params.h"
 
 #include "ompl/geometric/PathGeometric.h"
 #include "ompl/base/Path.h"
@@ -28,6 +29,7 @@
 #include <boost/math/constants/constants.hpp>
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
+ros::Publisher path_pub;
 
 class ValidityChecker : public ob::StateValidityChecker
 {
@@ -54,8 +56,13 @@ ob::OptimizationObjectivePtr getObjective(const ob::SpaceInformationPtr &si)
 {
     return ob::OptimizationObjectivePtr(new ob::PathLengthOptimizationObjective(si));
 }
-
-std::pair<ob::ProblemDefinitionPtr, ob::SpaceInformationPtr> construct(double start_x, double start_y, double goal_x, double goal_y)
+ob::OptimizationObjectivePtr getThreshold(const ob::SpaceInformationPtr &si, double threshold)
+{
+    ob::OptimizationObjectivePtr obj(new ob::PathLengthOptimizationObjective(si));
+    obj->setCostThreshold(ob::Cost(threshold));
+    return obj;
+}
+std::pair<ob::ProblemDefinitionPtr, ob::SpaceInformationPtr> construct(double start_x, double start_y, double goal_x, double goal_y, double threshold)
 {
     ob::StateSpacePtr space(new ob::RealVectorStateSpace(2));
     space->as<ob::RealVectorStateSpace>()->setBounds(0.0, 100.0);
@@ -73,30 +80,83 @@ std::pair<ob::ProblemDefinitionPtr, ob::SpaceInformationPtr> construct(double st
 
     ob::ProblemDefinitionPtr pdef(new ob::ProblemDefinition(si));
     pdef->setStartAndGoalStates(start, goal);
-    pdef->setOptimizationObjective(getObjective(si));
+    //pdef->setOptimizationObjective(getObjective(si));
+    pdef->setOptimizationObjective(getThreshold(si, threshold));
     return std::make_pair(pdef, si);
 }
 
-
-
-
-bool execute(path_planner::input_params::Request  &req,
-         path_planner::input_params::Response &res)
+bool execute(path_planning::input_params::Request &req,
+             path_planning::input_params::Response &res)
 {
-  return true;
+    double dyn_range = 5, dyn_threshold = 75;
+    double computation_time = 2.0;
+    auto pdef = construct(req.start_x, req.start_y, req.end_x, req.end_y, dyn_threshold);
+    og::RRTstar *rrt = new og::RRTstar(pdef.second);
+    rrt->setRange(dyn_range);
+
+    ob::PlannerPtr optimizingPlanner(rrt);
+
+    optimizingPlanner->setProblemDefinition(pdef.first);
+    optimizingPlanner->setup();
+    double initial_time = ros::Time::now().toSec();
+    ob::PlannerStatus solved = optimizingPlanner->solve(computation_time);
+    res.elapsedTime = ros::Time::now().toSec() - initial_time;
+    if (solved)
+    {
+        std::cout
+            << optimizingPlanner->getName()
+            << " found a solution of length "
+            << pdef.first->getSolutionPath()->length()
+            << " with an optimization objective value of "
+            << pdef.first->getSolutionPath()->cost(pdef.first->getOptimizationObjective()) << std::endl
+            << pdef.first->getSolutionCount() << std::endl;
+        ob::PlannerSolution *foundPaths = new ob::PlannerSolution(pdef.first->getSolutionPath());
+
+        pdef.first->getSolutionPath().get()->print(std::cout);
+        og::PathGeometric path(dynamic_cast<const og::PathGeometric &>(*pdef.first->getSolutionPath()));
+        const std::vector<ob::State *> &states = path.getStates();
+        ob::State *state;
+        nav_msgs::Path nav_path;
+        nav_path.header.frame_id = "map";
+        geometry_msgs::PoseStamped pose;
+        for (size_t i = 0; i < states.size(); ++i)
+        {
+            state = states[i]->as<ob::State>();
+            //nav_path.poses
+            double x, y;
+            pose.pose.position.x = state->as<ob::RealVectorStateSpace::StateType>()->values[0];
+            pose.pose.position.y = state->as<ob::RealVectorStateSpace::StateType>()->values[1];
+            pose.header.frame_id = "base_link";
+            nav_path.poses.push_back(pose);
+            //std::cout << x << " " << y << std::endl;
+        }
+
+        if (pdef.first->getSolutionPath()->length() > dyn_threshold)
+        {
+            res.isFound = false;
+            res.elapsedTime = 0.0;
+            return true;
+        }
+
+        path_pub.publish(nav_path);
+        res.isFound = true;
+        return true;
+    }
+    std::cout << "No solution found." << std::endl;
+    res.elapsedTime = 0.0;
+    res.isFound = true;
+    return true;
 }
-
-
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "path_planner");
     ros::NodeHandle nh;
-    double computation_time = 1.0;
+    path_pub = nh.advertise<nav_msgs::Path>("Path_Publisher", 1000);
     visualization_msgs::Marker obstacles[10];
     for (int i = 0; i < 10; i++)
     {
-        obstacles[i].header.frame_id = "frame_obstacle" + i;
+        obstacles[i].header.frame_id = "base_link";
         obstacles[i].header.stamp = ros::Time();
         obstacles[i].ns = "obstacle";
         obstacles[i].id = i;
@@ -119,55 +179,10 @@ int main(int argc, char **argv)
     }
 
     ros::Publisher obstacles_pub = nh.advertise<visualization_msgs::Marker>("Obstacles_Publisher", 1000);
-    ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("Path_Publisher", 1000);
     //ros::Rate loop_rate(10);
-    ros::ServiceServer service = n.advertiseService("input_params", execute);
+    ros::ServiceServer service = nh.advertiseService("input_params", execute);
     ROS_INFO("path_planner node has been started.");
     ros::spin();
-
-
-
-    auto pdef = construct(0.0, 0.0, 50.0, 50.0);
-    og::RRTstar *rrt = new og::RRTstar(pdef.second);
-    rrt->setRange(5.0);
-
-    ob::PlannerPtr optimizingPlanner(rrt);
-
-    optimizingPlanner->setProblemDefinition(pdef.first);
-    optimizingPlanner->setup();
-    ob::PlannerStatus solved = optimizingPlanner->solve(computation_time);
-    if (solved)
-    {
-        // Output the length of the path found
-        std::cout
-            << optimizingPlanner->getName()
-            << " found a solution of length "
-            << pdef.first->getSolutionPath()->length()
-            << " with an optimization objective value of "
-            << pdef.first->getSolutionPath()->cost(pdef.first->getOptimizationObjective()) << std::endl
-            << pdef.first->getSolutionCount() << std::endl;
-        ob::PlannerSolution *foundPaths = new ob::PlannerSolution(pdef.first->getSolutionPath());
-
-        pdef.first->getSolutionPath().get()->print(std::cout);
-        og::PathGeometric path(dynamic_cast<const og::PathGeometric &>(*pdef.first->getSolutionPath()));
-        const std::vector<ob::State *> &states = path.getStates();
-        ob::State *state;
-        nav_msgs::Path nav_path;
-        geometry_msgs::PoseStamped pose;
-        for (size_t i = 0; i < states.size(); ++i)
-        {
-            state = states[i]->as<ob::State>();
-            //nav_path.poses
-            double x, y;
-            pose.pose.position.x = state->as<ob::RealVectorStateSpace::StateType>()->values[0];
-            pose.pose.position.y = state->as<ob::RealVectorStateSpace::StateType>()->values[1];
-            nav_path.poses.push_back(pose);
-            //std::cout << x << " " << y << std::endl;
-        }
-        path_pub.publish(nav_path);
-    }
-    else
-        std::cout << "No solution found." << std::endl;
 
     return 0;
 }
